89a8216719cdde9387a1f9987bf8047f71f117a4
Suman Kumar <sumankumar748200@gmail.com>
2025-11-29 12:31:56 +0530

    Updated project files

--- app/api/admin/login/route.js
+++ app/api/admin/login/route.js
@@ -1,4 +1,3 @@
-
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import Jwt from "jsonwebtoken";
@@ -8,49 +7,32 @@ import Admin from "@/app/models/Admin";
 export const POST = async (req) => {
   await ConnectDb();
 
-  const headers = {
-    "Access-Control-Allow-Origin": "*", // or specify your frontend domain
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  };
-
-  if (req.method === "OPTIONS") {
-    return NextResponse.json({}, { headers });
-  }
-
   try {
-    const records = await req.json();
-    const { username, password } = records;
+    const { username, password } = await req.json();
 
     const admin = await Admin.findOne({ username });
     if (!admin) {
-      return NextResponse.json({ msg: "Invalid Username" }, { status: 400, headers });
+      return NextResponse.json({ msg: "Invalid username", success: false }, { status: 400 });
     }
 
     const validPassword = await bcrypt.compare(password, admin.password);
     if (!validPassword) {
-      return NextResponse.json(
-        { msg: "Invalid username or password" },
-        { status: 400, headers }
-      );
+      return NextResponse.json({ msg: "Invalid username or password", success: false }, { status: 400 });
     }
 
+    // JWT token
     const tokenData = {
       id: admin._id,
       username: admin.username,
     };
 
-    const token = Jwt.sign(tokenData, process.env.NEXTAUTH_SECRET, { expiresIn: "1h" });
+    const token = Jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: "1h" });
 
-    const response = NextResponse.json(
-      { msg: "Login Successful", success: true },
-      { headers }
-    );
-
+    const response = NextResponse.json({ msg: "Login Successful", success: true });
     response.cookies.set("token", token, { httpOnly: true, path: "/" });
 
     return response;
   } catch (err) {
-    return NextResponse.json({ error: err.message }, { status: 500, headers });
+    return NextResponse.json({ msg: "Something went wrong", error: err.message }, { status: 500 });
   }
 };
